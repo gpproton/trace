dc708ad0fb164ce91612b34f35bc93d53aee20d8
radioActive DROID <me@godwin.dev>
2023-12-22 22:28:59 +0100

    added frontend proxies

--- src/Frontend/Program.cs
+++ src/Frontend/Program.cs
@@ -6,9 +6,14 @@ builder.RegisterDefaults();
 builder.AddRedisOutputCache("cache");
 builder.Services.RegisterDefaultServices();
 
+builder.Services.AddReverseProxy()
+    .LoadFromConfig(builder.Configuration.GetSection("FrontendProxies"))
+    .AddServiceDiscoveryDestinationResolver();
+
 var app = builder.Build();
 
 app.RegisterDefaults();
+app.MapReverseProxy();
 app.MapFallbackToFile("index.html");
 
 app.Run();
