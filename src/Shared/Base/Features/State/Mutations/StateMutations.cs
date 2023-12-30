76335d987e3e3dc9df8ba0e8e7e1ef3594d99ad6
radioActive DROID <me@godwin.dev>
2023-12-30 09:03:58 +0100

    cleanup dangling files

--- src/Shared/Base/Features/State/Mutations/StateMutations.cs
+++
@@ -1,9 +0,0 @@
-using HotChocolate.Types;
-using Trace.Shared.Base.GraphqlRoot;
-
-namespace Trace.Shared.Base.Features.State.Mutations;
-
-[ExtendObjectType(typeof(MutationRoot))]
-public class StateMutations {
-    public int TriggerCheck(int input) => input;
-}
--- src/Shared/Base/GraphqlRoot/QueryRoot.cs
+++
@@ -1,8 +0,0 @@
-namespace Trace.Shared.Base.GraphqlRoot;
-
-/// <summary>
-/// This entity acts as our root node, from here we hang each of entities off it via queries
-/// </summary>
-public class QueryRoot {
-    public string Version => "Trace";
-}
