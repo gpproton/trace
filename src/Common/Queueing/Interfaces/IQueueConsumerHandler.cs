ad1e849570d63cd68497102a023da4f5b0a41950
radioActive DROID <me@godwin.dev>
2023-12-31 02:16:27 +0100

    added queue consumer handler

---
+++ src/Common/Queueing/Interfaces/IQueueConsumerHandler.cs
@@ -0,0 +1,7 @@
+﻿namespace Trace.Common.Queueing.Interfaces;
+
+internal interface IQueueConsumerHandler<TMessageConsumer, TQueueMessage> where TMessageConsumer : IQueueConsumer<TQueueMessage> where TQueueMessage : class, IQueueMessage {
+    void RegisterQueueConsumer();
+
+    void CancelQueueConsumer();
+}
