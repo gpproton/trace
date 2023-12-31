943c65ff2dc9a4a1b2e53355f0efa09eb7a5d688
radioActive DROID <me@godwin.dev>
2023-12-31 02:17:02 +0100

    enable queue consumer registration

---
+++ src/Common/Queueing/Implementation/QueueConsumerRegistratorService.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Trace.Common.Queueing.Interfaces;
+
+namespace Trace.Common.Queueing.Implementation;
+
+internal class QueueConsumerRegistratorService<TMessageConsumer, TQueueMessage>(ILogger<QueueConsumerRegistratorService<TMessageConsumer, TQueueMessage>> logger,
+    IServiceProvider serviceProvider) : IHostedService where TMessageConsumer : IQueueConsumer<TQueueMessage> where TQueueMessage : class, IQueueMessage {
+    private IQueueConsumerHandler<TMessageConsumer, TQueueMessage>? _consumerHandler;
+    private IServiceScope? _scope;
+
+    public Task StartAsync(CancellationToken cancellationToken) {
+        logger.LogInformation($"Registering {typeof(TMessageConsumer).Name} as Consumer for Queue {typeof(TQueueMessage).Name}");
+        _scope = serviceProvider.CreateScope();
+
+        _consumerHandler = _scope.ServiceProvider.GetRequiredService<IQueueConsumerHandler<TMessageConsumer, TQueueMessage>>();
+        _consumerHandler.RegisterQueueConsumer();
+
+        return Task.CompletedTask;
+    }
+
+    public Task StopAsync(CancellationToken cancellationToken) {
+        logger.LogInformation($"Stop {nameof(QueueConsumerRegistratorService<TMessageConsumer, TQueueMessage>)}: Canceling {typeof(TMessageConsumer).Name} as Consumer for Queue {typeof(TQueueMessage).Name}");
+
+        _consumerHandler?.CancelQueueConsumer();
+        _scope?.Dispose();
+
+        return Task.CompletedTask;
+    }
+}
---
+++ src/Common/Queueing/Interfaces/IQueueConsumer.cs
@@ -0,0 +1,5 @@
+﻿namespace Trace.Common.Queueing.Interfaces;
+
+public interface IQueueConsumer<in TQueueMessage> where TQueueMessage : class, IQueueMessage {
+    Task ConsumeAsync(TQueueMessage message);
+}
