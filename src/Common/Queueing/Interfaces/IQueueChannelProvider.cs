5a23ba095e1f7fc184f3947cbcc545d80aa91e48
radioActive DROID <me@godwin.dev>
2023-12-31 02:14:21 +0100

    added queue channel provider

---
+++ src/Common/Queueing/Interfaces/IQueueChannelProvider.cs
@@ -0,0 +1,7 @@
+﻿namespace Trace.Common.Queueing.Interfaces;
+
+/// <summary>
+/// A channel provider that Declares and Binds a specific queue
+/// </summary>
+internal interface IQueueChannelProvider<in TQueueMessage> : IChannelProvider where TQueueMessage : IQueueMessage {
+}
