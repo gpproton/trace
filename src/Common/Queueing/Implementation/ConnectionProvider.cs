7bfcefc7bd352f71065e09d25f3ebf930ec520bb
radioActive DROID <me@godwin.dev>
2023-12-31 02:13:40 +0100

    added connection provider

---
+++ src/Common/Queueing/Implementation/ConnectionProvider.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Extensions.Logging;
+using RabbitMQ.Client;
+using Trace.Common.Queueing.Interfaces;
+
+namespace Trace.Common.Queueing.Implementation;
+
+internal sealed class ConnectionProvider(ILogger<ConnectionProvider> logger, IAsyncConnectionFactory connectionFactory) : IDisposable, IConnectionProvider {
+    private IConnection? _connection;
+
+    public IConnection GetConnection() {
+        if (_connection is null || !_connection.IsOpen) {
+            logger.LogDebug("Open RabbitMQ connection");
+            _connection = connectionFactory.CreateConnection();
+        }
+
+        return _connection;
+    }
+
+    public void Dispose() {
+        try {
+            if (_connection != null && _connection.IsOpen) {
+                logger.LogDebug("Closing the connection");
+                _connection?.Close();
+                _connection?.Dispose();
+            }
+        }
+        catch (Exception ex) {
+            logger.LogCritical(ex, "Cannot dispose RabbitMq channel or connection");
+        }
+
+    }
+}
