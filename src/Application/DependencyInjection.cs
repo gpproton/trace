e034809f89f4a76a37d1a19b353a790da5208756
radioActive DROID <me@godwin.dev>
2024-01-01 04:05:12 +0100

    Update DependencyInjection.cs

--- src/Application/DependencyInjection.cs
+++ src/Application/DependencyInjection.cs
@@ -1,4 +1,18 @@
-﻿using System.Reflection;
+﻿// Copyright 2023 - 2024 Godwin peter .O (me@godwin.dev)
+//
+// Licensed under the Business Source License 1.1 and Trace License;
+// you may not use this file except in compliance with the License.
+//
+//     https://mariadb.com/bsl11
+//     https://trace.ng/license
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System.Reflection;
 using FluentValidation;
 using Microsoft.Extensions.DependencyInjection;
 
