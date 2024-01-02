eeccbf5406d0841e61d48402e3a4b28578c88778
radioActive DROID <me@godwin.dev>
2024-01-02 03:20:04 +0100

    Update Nodes.cs

--- src/ServiceDefaults/Nodes.cs
+++ src/ServiceDefaults/Nodes.cs
@@ -1,12 +1,19 @@
-// Copyright 2022 - 2023 Godwin peter .O (me@godwin.dev)
-//
-// Licensed under the Reciprocal Public License (RPL-1.5) and Trace License;
+// Copyright (c) 2023 - 2024 drolx Solutions
+// 
+// Licensed under the Business Source License 1.1 and Trace License;
 // you may not use this file except in compliance with the License.
+//     https://mariadb.com/bsl11
+//     https://trace.ng/license
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+// 
+// Author: Godwin peter .O (me@godwin.dev)
+// Created At: Sunday, 31st Dec 2023
+// Modified By: Godwin peter .O
+// Modified At: Mon Jan 01 2024
 
 namespace Trace.ServiceDefaults;
 
