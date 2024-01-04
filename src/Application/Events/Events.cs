ed1c8f9ea048d344089bfc17b6c1170258620257
radioActive DROID <me@godwin.dev>
2024-01-04 03:09:17 +0100

    added location and device entities

---
+++ src/Application/Events/Events.cs
@@ -0,0 +1,33 @@
+// Copyright (c) 2023 - 2024 drolx Solutions
+//
+// Licensed under the Business Source License 1.1 and Trace License
+// you may not use this file except in compliance with the License.
+// Change License: Reciprocal Public License 1.5
+//     https://mariadb.com/bsl11
+//     https://opensource.org/license/rpl-1-5
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// Author: Godwin peter .O (me@godwin.dev)
+// Created At: Wednesday, 3rd Jan 2024
+// Modified By: Godwin peter .O
+// Modified At: Wed Jan 03 2024
+
+using Trace.Application.Core;
+
+namespace Trace.Application.Events;
+
+public class Events : TenantEntity<Guid> {
+    public DateTimeOffset Time { get; set; }
+
+    public DateTimeOffset ServerTime { get; set; }
+
+    public Guid? BeaconId { get; set; }
+
+    public Guid? PositionId { get; set; }
+
+    public Guid? LocationId { get; set; }
+}
