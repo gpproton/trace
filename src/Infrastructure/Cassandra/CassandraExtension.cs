b783704c0230fb04c8a67d93fe95f5f4f7a68ba6
radioActive DROID <me@godwin.dev>
2024-01-11 17:57:29 +0100

    created cassandra setup extension

--- src/Infrastructure/Cassandra/CassandraExtension.cs
+++ src/Infrastructure/Cassandra/CassandraExtension.cs
@@ -16,12 +16,51 @@
 // Modified By: Godwin peter .O
 // Modified At: Tue Jan 02 2024
 
+using Cassandra;
+using Cassandra.Mapping;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using Trace.Application.Device;
+using Trace.Infrastructure.Cassandra.Implementation;
+using Trace.Infrastructure.Cassandra.Interfaces;
 
 namespace Trace.Infrastructure.Cassandra;
 
 public static class CassandraExtension {
     public static IServiceCollection RegisterCassandraInfrastructure(this IServiceCollection services) {
+        const string keyspace = CanssandraConst.Keyspace;
+
+        // Registers entities mapping
+        services.AddSingleton(_ => new CassandraOptions {
+            Keyspace = keyspace,
+            Config = [
+                DevicePosition.GetConfig(keyspace),
+            ]
+        });
+
+        var scope = services.BuildServiceProvider();
+        var config = services.BuildServiceProvider().GetRequiredService<IConfiguration>();
+        var cassandraOptions = scope.GetRequiredService<CassandraOptions>();
+
+        services.AddOptions<CassandraConfig>().Bind(config.GetSection(CassandraConfig.Key));
+        services.AddSingleton<ICluster>(provider => {
+            var conf = provider.GetRequiredService<IOptions<CassandraConfig>>();
+            var queryOptions = new QueryOptions().SetConsistencyLevel(ConsistencyLevel.One);
+
+            return Cluster.Builder()
+                .AddContactPoint(conf.Value.Host)
+                .WithPort(conf.Value.Port)
+                .WithCompression(CompressionType.LZ4)
+                .WithCredentials(conf.Value.Username, conf.Value.Password)
+                .WithQueryOptions(queryOptions)
+                .WithRetryPolicy(new LoggingRetryPolicy(new DefaultRetryPolicy()))
+                .Build();
+        });
+        services.AddScoped<ICassandraProvider, CassandraProvider>();
+
+        MappingConfiguration.Global.Define(cassandraOptions.Config);
+        services.AddHostedService<CassandraHostedService>();
 
         return services;
     }
