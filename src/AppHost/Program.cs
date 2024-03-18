087dee066e0e5714b3fe7c96bd81e00a52b4526d
Godwin peter. O <me@godwin.dev>
2024-03-18 15:30:42 +0100

    updated aspire

--- src/AppHost/Program.cs
+++ src/AppHost/Program.cs
@@ -1,38 +1,39 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-#if !DEBUG
-var cache = builder.AddRedisContainer("cache", port: 6379);
-var messaging = builder.AddRabbitMQContainer("messaging", port: 5672, password: "guest");
-var db = builder.AddPostgresContainer("db", port: 5432, password: "trace")
-.WithAnnotation(new ContainerImageAnnotation { Image = "postgis/postgis", Tag = "15-3.3" })
+var cache = builder.AddRedis("cache", port: 6379).PublishAsContainer();
+var messaging = builder.AddRabbitMQ("messaging", port: 5672).WithEnvironment("RABBITMQ_DEFAULT_PASS", "guest");
+var db = builder.AddPostgres("db", port: 5432, password: "trace")
+.WithImage("postgis/postgis")
+.WithImageTag("15-3.3")
 .AddDatabase("trace");
 var scylladb = builder.AddContainer("scylladb", "scylladb/scylla", "5.4")
-    .WithServiceBinding(containerPort: 9042, scheme: "tcp", name: "default")
-    .WithServiceBinding(containerPort: 9160, scheme: "tcp", name: "thrift");
-#endif
+    .WithEndpoint(containerPort: 9042, scheme: "tcp", name: "default")
+    .WithEndpoint(containerPort: 9160, scheme: "tcp", name: "thrift");
 
-var coreService = builder.AddProject<Projects.Trace_Service_Core>("service-core");
-var integrationService = builder.AddProject<Projects.Trace_Service_Integration>("service-integration");
-var routingService = builder.AddProject<Projects.Trace_Service_Navigation>("service-navigation");
-var gateway = builder.AddProject<Projects.Trace_Gateway>("gateway")
-    .WithReference(coreService)
-    .WithReference(integrationService)
-    .WithReference(routingService);
-var manager = builder.AddProject<Projects.Trace_Manager>("manager")
-    .WithReference(integrationService)
-    .WithReference(routingService);
-var frontend = builder.AddProject<Projects.Trace_Frontend>("frontend")
-    .WithReference(gateway)
-    .WithReference("geocoding", new Uri("https://nominatim.openstreetmap.org"))
-    .WithReference("routing", new Uri("https://valhalla.openstreetmap.de"));
+// var coreService = builder.AddProject<Projects.Trace_Service_Core>("service-core")
+// .WithReference(cache)
+// .WithReference(messaging)
+// .WithReference(db);
 
-#if !DEBUG
-coreService.WithReference(cache).WithReference(messaging).WithReference(db);
-integrationService.WithReference(cache).WithReference(messaging).WithReference(db);
-routingService.WithReference(cache).WithReference(messaging).WithReference(db);
-gateway.WithReference(cache);
-manager.WithReference(cache).WithReference(messaging).WithReference(db);
-frontend.WithReference(cache);
-#endif
+// var integrationService = builder.AddProject<Projects.Trace_Service_Integration>("service-integration");
+// var routingService = builder.AddProject<Projects.Trace_Service_Navigation>("service-navigation");
+// var gateway = builder.AddProject<Projects.Trace_Gateway>("gateway")
+//     .WithReference(coreService)
+//     .WithReference(integrationService)
+//     .WithReference(routingService);
+// var manager = builder.AddProject<Projects.Trace_Manager>("manager")
+//     .WithReference(integrationService)
+//     .WithReference(routingService);
+// var frontend = builder.AddProject<Projects.Trace_Frontend>("frontend")
+//     .WithReference(gateway)
+//     .WithReference("geocoding", new Uri("https://nominatim.openstreetmap.org"))
+//     .WithReference("routing", new Uri("https://valhalla.openstreetmap.de"));
+
+// coreService.WithReference(cache).WithReference(messaging).WithReference(db);
+// integrationService.WithReference(cache).WithReference(messaging).WithReference(db);
+// routingService.WithReference(cache).WithReference(messaging).WithReference(db);
+// gateway.WithReference(cache);
+// manager.WithReference(cache).WithReference(messaging).WithReference(db);
+// frontend.WithReference(cache);
 
 builder.Build().Run();
